f7e1feb9b8ff714743a8cde3f79b4b22ffcc2c67
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2025-09-30 00:25:10 -0400

    Created new project - making an AI to predict trends and patterns from a .csv file from scratch.

---
+++ 4-Personal_AI/Code/main.py
@@ -0,0 +1,22 @@
+import csv
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+fp = "Data/ex.csv" # fp for file path
+df = pd.read_csv(fp) # df for data file
+
+def main():
+    # plot_data(read_csv(fp)[0])
+    sns.regplot(
+        data = df, 
+        x = 'Units_Sold', 
+        y = 'Revenue', 
+        order = 3,
+        ci = None)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
